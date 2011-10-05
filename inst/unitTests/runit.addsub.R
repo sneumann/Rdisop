9e72e33ea707901c7e0490b0cb84f5f6e2bd3332
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-05 20:10:31 +0000

    fix test, now real segf
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rdisop@58807 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.addsub.R
+++ inst/unitTests/runit.addsub.R
@@ -1,9 +1,9 @@
 test.subformula1 <- function() {
-   checkEquals(subMolecules("C5H10NO4", "H")$formula=="C5H9NO4")
+   checkEquals(subMolecules("C5H10NO4", "H")$formula, "C5H9NO4")
 }
 
 
 test.subformula2 <- function() {
-   checkEquals(subMolecules("C14H23N4O8P2S", "C3H4O3")$formula=="C11H19N4O5P2S")
+   checkEquals(subMolecules("C14H23N4O8P2S", "C3H4O3")$formula, "C11H19N4O5P2S")
 }
 
