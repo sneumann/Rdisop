7fb42b4c7b4c8240a0dc663e23a4d16da2eef312
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-05 20:10:45 +0000

    loosened tolerance for exact mass check
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rdisop@58809 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.keggexamples.R
+++ inst/unitTests/runit.keggexamples.R
@@ -1,13 +1,16 @@
 
 test.cid46173807 <- function() {
   checkEqualsNumeric(852.354928,
-                     getMolecule("C49H56FeN4O6",initializePSE(),z=0)$exactmass) 
+                     getMolecule("C49H56FeN4O6",initializePSE(),z=0)$exactmass,
+		     tolerance = 0.00005) 
 
 }
 
 test.cid24892761 <- function() {
   checkEqualsNumeric(447.089515,
-                     getMolecule("C15H23N6O5Se+",c(initializePSE(),initializeCharges()),z=1)$exactmass) 
+                     getMolecule("C15H23N6O5Se+",c(initializePSE(),initializeCharges()),z=1)$exactmass,
+		     tolerance = 0.00005) 
+
 
 }
 
