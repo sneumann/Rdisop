7ec11a144d6b0655c980dd39df4830c220722b14
s.neumann <s.neumann@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-23 08:00:27 +0000

    some more unit tests
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Rdisop@63003 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runit.addsub.R
+++ inst/unitTests/runit.addsub.R
@@ -1,3 +1,12 @@
+test.addformula1 <- function() {
+   checkEquals(addMolecules("H", "H")$formula, "H2")   
+}
+
+test.addformula2 <- function() {
+   checkEqualsNumeric(addMolecules("C2H6O", "C2H6O")$exactmass, 2*46.04, tolerance=0.1)   
+}
+
+
 test.subformula1 <- function() {
    checkEquals(subMolecules("C5H10NO4", "H")$formula, "C5H9NO4")
 }
@@ -7,3 +16,6 @@ test.subformula2 <- function() {
    checkEquals(subMolecules("C14H23N4O8P2S", "C3H4O3")$formula, "C11H19N4O5P2S")
 }
 
+test.subformula1 <- function() {
+   checkEquals(subMolecules("CH", "H")$formula, "C")
+}
