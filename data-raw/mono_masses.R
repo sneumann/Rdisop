f600ea6cc9da253c02e51b79d405e8365d2e7fae
janlisec <jan.lisec@bam.de>
2025-01-22 12:46:32 +0100

    Fix for issue #22 using a new elemental definition set and implementing a test for old and new set.

--- data-raw/mono_masses.R
+++ data-raw/mono_masses.R
@@ -1,7 +1,7 @@
 # to provide a fast way calculating the mono-isotopic mass of an element, here a
 # named vector containing the main isotopes of all PSE elements is prepared to 
 # be accessed by Rdisop::mono_masses
-ele <- initializePSE()
+ele <- initializePSE(method = "NIST")
 mono_masses <- sort(sapply(ele, function(x) {
     m <- x$mass + x$isotope$mass + (1:length(x$isotope$mass))-1
     m <- m[which.max(x$isotope$abundance)]
